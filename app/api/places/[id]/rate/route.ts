b0e1c8a56ba8124176526fdfa072aec071ed8df7
soynerd <mesoynerd@gmail.com>
2026-02-27 23:15:09 +0530

    PLaces APIs

---
+++ app/api/places/[id]/rate/route.ts
@@ -0,0 +1,45 @@
+import { NextRequest, NextResponse } from "next/server"
+import { prisma } from "@/lib/prismaClient"
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/lib/authOptions" // <-- important
+
+export async function POST(
+    req: NextRequest,
+    context: { params: Promise<{ id: string }> }
+) {
+    const { id } = await context.params
+
+    const session = await getServerSession(authOptions)
+
+    if (!session?.user?.id) {
+        return NextResponse.json(
+            { error: "Unauthorized" },
+            { status: 401 }
+        )
+    }
+
+    const userId = Number(session.user.id)
+    const placeId = Number(id)
+
+    const { rating } = await req.json()
+
+    await prisma.placeRating.upsert({
+        where: {
+            userId_placeId: {
+                userId,
+                placeId,
+            },
+        },
+        update: { rating },
+        create: { rating, userId, placeId },
+    })
+
+    await prisma.user.update({
+        where: { id: userId },
+        data: {
+            totalVisits: { increment: 1 },
+        },
+    })
+
+    return NextResponse.json({ success: true })
+}
