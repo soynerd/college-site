6c2c0cea491e6b7c279726a40d2756a762b82d0d
soynerd <mesoynerd@gmail.com>
2026-03-25 22:12:54 +0530

    Removed Testing Lines

--- app/api/cron/refill-reviews/route.ts
+++ app/api/cron/refill-reviews/route.ts
@@ -4,21 +4,9 @@ import { prisma } from "@/lib/prismaClient";
 export async function GET(req: Request) {
     try {
         const authHeader = req.headers.get("authorization");
-        console.log("Auth Header:", authHeader); // Debugging line
-        console.log("Expected Header:", `Bearer ${process.env.CRON_SECRET}`); // Debugging line
         if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
             return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
         }
-
-        const temp = await prisma.user.findMany({
-            select: {
-                email: true,
-            },
-            where: {
-                totalReviewsLeft: { lt: 6 },
-            },
-        });
-        console.log(temp)
 
         await prisma.user.updateMany({
             where: {
